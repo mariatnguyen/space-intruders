6a0a5b0001a4970f14bcdb77712966c40d279f27
mariatnguyen <mariatnguyen@mail.com>
2021-09-19 01:08:33 -0700

    add animations 👽

--- src/Play/Play.js
+++ src/Play/Play.js
@@ -8,16 +8,72 @@ class Play extends PureComponent {
     super(props);
     this.updateBarriers = this.updateBarriers.bind(this);
     this.state = {
-      barrierHealth: [1,1,1]
+      barriers: [1, 1, 1],
+      aliens: [[1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1]]
     }
   }
 
   componentDidMount() {
     this.movePlayer();
+    this.updateAliens();
+  }
+
+  updateAliens() {
+    let row1 = document.createElement("div");
+    row1.className = "play__row1";
+    for(let i = 0; i < this.state.aliens[0].length; i++) { 
+      let alien = document.createElement("div");
+      alien.className = "alien";
+      row1.append(alien);
+      if(this.state.aliens[0][i]) {
+        let image = document.createElement("img");
+        image.src = chars.alien1.src;
+        alien.append(image);
+        let secondImage = document.createElement("img");
+        secondImage.src = chars.alien1['src-open'];
+        alien.append(secondImage);
+      }
+    }
+    document.querySelector('.play__intruders-area').append(row1);
+
+
+    let row2 = document.createElement("div");
+    row2.className = "play__row2";
+    for(let i = 0; i < this.state.aliens[1].length; i++) { 
+      let alien = document.createElement("div");
+      alien.className = "alien";
+      row2.append(alien);
+      if(this.state.aliens[1][i]) {
+        let image = document.createElement("img");
+        image.src = chars.alien2.src;
+        alien.append(image);
+        let secondImage = document.createElement("img");
+        secondImage.src = chars.alien2['src-open'];
+        alien.append(secondImage);
+      }
+    }
+    document.querySelector('.play__intruders-area').append(row2);
+
+
+    let row3 = document.createElement("div");
+    row3.className = "play__row3";
+    for(let i = 0; i < this.state.aliens[2].length; i++) { 
+      let alien = document.createElement("div");
+      alien.className = "alien";
+      row3.append(alien);
+      if(this.state.aliens[2][i]) {
+        let image = document.createElement("img");
+        image.src = chars.alien3.src;
+        alien.append(image);
+        let secondImage = document.createElement("img");
+        secondImage.src = chars.alien3['src-open'];
+        alien.append(secondImage);
+      }
+    }
+    document.querySelector('.play__intruders-area').append(row3);
   }
 
   updateBarriers() {
-
   }
 
   movePlayer() {
@@ -31,14 +87,27 @@ class Play extends PureComponent {
     window.addEventListener('keydown', (e) => {
       switch (e.key) {
         case 'ArrowLeft':
-          player.style.left = parseInt(player.style.left) - moveBy + 'px';
+          player.style.left === "0px" ? player.style.left = "0px" : player.style.left = parseInt(player.style.left) - moveBy + 'px';
           break;
         case 'ArrowRight':
-          player.style.left = parseInt(player.style.left) + moveBy + 'px';
+          player.style.left === "440px" ? player.style.left = "440px" : player.style.left = parseInt(player.style.left) + moveBy + 'px';
           break;
         case 'ArrowUp':
         case " ":
         case 'Spacebar':
+          let laser = document.createElement("div");
+          laser.className = "laser";
+          laser.style.left = player.style.left;
+          document.querySelector('.play__player-area').appendChild(laser);
+          /*let position = 0;
+
+          let aim = setInterval(shootLaser, 1000);
+
+          function shootLaser() {
+            if(player.style.left === "0px") {
+              clearInterval(aim);
+            }
+          }*/
           break;
         default:
           break;
@@ -64,7 +133,7 @@ class Play extends PureComponent {
           <div className="play__barrier">
             <img
               id="barrier1"
-              src={barriers['barrier' + this.state.barrierHealth[0]].src}
+              src={barriers['barrier' + this.state.barriers[0]].src}
               width="50"
               height="31"
               alt="barrier"
@@ -73,7 +142,7 @@ class Play extends PureComponent {
           <div className="play__barrier">
             <img
               id="barrier2"
-              src={barriers['barrier' + this.state.barrierHealth[1]].src}
+              src={barriers['barrier' + this.state.barriers[1]].src}
               width="50"
               height="31"
               alt="barrier"
@@ -82,7 +151,7 @@ class Play extends PureComponent {
           <div className="play__barrier">
             <img
               id="barrier3"
-              src={barriers['barrier' + this.state.barrierHealth[2]].src}
+              src={barriers['barrier' + this.state.barriers[2]].src}
               width="50"
               height="31"
               alt="barrier"
